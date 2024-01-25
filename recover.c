b662f1da5d80c558452370b50951c1dabf62528c
MichaelAlbornozWebb <143023923+MichaelAlbornozWebb@users.noreply.github.com>
2024-01-25 09:55:43 -0800

    recover.c

--- recover.c
+++ recover.c
@@ -2,28 +2,31 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-typedef uint8_t BYTE;
+//Michael Albornoz
+//Recover
+
+typedef uint8_t BYTE; //instilize BYTE constant
 
 int main(int argc, char *argv[])
 {
-    BYTE buffer[512];
+    BYTE buffer[512]; //make 8 byte constant
 
-    if(argc!=2)
+    if(argc!=2) //kills program when ran incorrectly
     {
         printf("Usage: ./recover IMAGE\n");
         return 1;
     }
 
-    FILE *f = fopen(argv[1], "r");
-    int counter = 0;
-    char filename[8];
+    FILE *f = fopen(argv[1], "r"); //opens file
+    int counter = 0; //counter for naming files
+    char filename[8]; //naming file
 
     if(f==NULL)
     {
         return 2;
     }
 
-    FILE *img;
+    FILE *img; //file
 
     while(fread(buffer, sizeof(BYTE)*512, 1, f)==1)
     {
@@ -33,22 +36,25 @@ int main(int argc, char *argv[])
             {
                 if (buffer[2] == 0xff)
                 {
-                    if ((buffer[3] & 0xf0) == 0xe0)
+                    if ((buffer[3] & 0xf0) == 0xe0) //all if statement to detect JPG image
                     {
-                        sprintf(filename, "%03i.jpg", counter);
-                        counter = counter + 1;
-
-                        img = fopen(filename, "w");
-
-                        fwrite(buffer, sizeof(BYTE)*512, 1, img);
+                        sprintf(filename, "%03i.jpg", counter); //prints pixels on img
+                        counter = counter + 1; //counter
+                        img = fopen(filename, "w"); //opens files from line 38 to write onto
                     }
                 }
             }
         }
+
+        if (img != NULL)
+        {
+            fwrite(buffer, sizeof(BYTE)*512, 1, img); //prints JPG it on the file
+        }
+
     }
     fclose(img);
 
-    fclose(f);
+    fclose(f); //closes files
 
     return 0;
 }
